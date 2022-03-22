932e7312195d980358b2bb10866b1b1c785454d2
Dimas Ananda <traperwaze@gmail.com>
2022-03-22 21:00:41 +0700

    added script to create new vhost
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker-entrypoint.sh rabbitmq-server
+docker-entrypoint.sh rabbitmq-server &
+
+echo "Waiting on RabbitMQ to start"
+sleep 20
+
+echo "Creating new inothing vhost"
+rabbitmqctl add_vhost /inothing
